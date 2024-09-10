6c3fed4b7c349bbc19c253f847e06be68602bbe7
Joshalex1 <161536154+Joshalex1@users.noreply.github.com>
2024-09-09 23:07:30 -0700

    player controls

---
+++ JAlexander_IndependentProject/Assets/Scripts/playermove.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermove : MonoBehaviour
+{
+    public float speed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+
+    
+
+    void Update()
+    {
+        float hVal = Input.GetAxis("Horizontal");
+        float vVal = Input.GetAxis("Vertical");
+       
+        Vector3 movement = new Vector3(hVal, 0 , vVal) * speed * Time.deltaTime;
+
+     transform.Translate(movement);
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+
+        {
+            Vector3 jump = new Vector3(0, 3, 0 );
+            transform.Translate(jump);
+
+
+        }
+
+
+
+    }
+
+
+}
