cdee5ded6571725fd0e782121e5f785b733d7417
Joshalex1 <161536154+Joshalex1@users.noreply.github.com>
2024-10-22 05:03:43 -0700

    independent project

--- JAlexander_IndependentProject/Assets/Scripts/ArtifactCollector.cs
+++ JAlexander_IndependentProject/Assets/Scripts/ArtifactCollector.cs
@@ -7,12 +7,20 @@ public class ArtifactCollector : MonoBehaviour
     public GameObject[] artifacts;
     public GameObject door; // Reference to the door GameObject 
     private int collectedCount = 0;
+    public ParticleSystem collectEffect;
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Artifact"))
         {
             CollectArtifact(other.gameObject);
+
+            if (collectEffect != null)
+            {
+                ParticleSystem effect = Instantiate(collectEffect, transform.position, Quaternion.identity);
+                effect.Play();
+                Destroy(effect.gameObject, effect.main.duration);
+            }
         }
     }
 
